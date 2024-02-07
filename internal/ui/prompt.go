f457f8c76e92a255f00fc54e00ad3dd5f4d9b74d
Junhyeok Claud Choi <gwolves@gwolv.es>
2024-02-08 02:12:53 +0900

    feat: alias q=esc

--- internal/ui/prompt.go
+++ internal/ui/prompt.go
@@ -155,7 +155,7 @@ func (p *Prompt) keyboard(evt *tcell.EventKey) *tcell.EventKey {
 	case tcell.KeyRune:
 		p.model.Add(evt.Rune())
 
-	case tcell.KeyEscape:
+	case tcell.KeyEscape, KeyQ:
 		p.model.ClearText(true)
 		p.model.SetActive(false)
 
